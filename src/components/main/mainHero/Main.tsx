df0b500c66ff2c152314a5e62cfbad5970936640
GlebTc <bob88tikhonov@gmail.com>
2024-03-18 07:43:48 -0400

    Trying to fix main navigation buttons
    Removed reusable component on Main.tsx page and added the link directly into the Main.tsx

--- src/components/main/mainHero/Main.tsx
+++ src/components/main/mainHero/Main.tsx
@@ -1,6 +1,8 @@
 import HeroImage from "./HeroImage";
 import HeroText from "./HeroText";
 import HeroOverlay from "./HeroOverlay";
+import Link from "next/link";
+import { FaChevronCircleDown } from "react-icons/fa";
 import NextSectionScrollButton from "@/components/reusable/NextSectionScrollButton";
 
 const Main = () => {
@@ -13,7 +15,15 @@ const Main = () => {
       <HeroOverlay />
       <HeroText />
       <div className="hidden md:block relative max-w-[1200px] mx-auto">
-        <NextSectionScrollButton section="about" title="About Us" />
+      <Link
+      href={`#about`}
+      title={`Navigate to About Us section`}
+      aria-label={`Navigate to About Us section`}
+      className={`bg-red-600/80 px-4 py-1 text-white flex justify-center gap-2 items-center rounded-md animate-bounce absolute bottom-4 left-4 font-bold text-lg z-[20]`}
+    >
+      <FaChevronCircleDown size={25} />
+      <p>About Us</p>
+    </Link>
       </div>
     </div>
   );
