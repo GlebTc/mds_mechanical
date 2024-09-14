b6c131d25f72e0f70d22ef9f508bf2f8760b7db8
GlebTc <bob88tikhonov@gmail.com>
2024-09-14 14:22:11 -0400

    Added G4A onClick event

--- src/components/navbar/NavbarFixedBar.tsx
+++ src/components/navbar/NavbarFixedBar.tsx
@@ -1,9 +1,10 @@
-"use client";
-import { useState } from "react";
-import { FaPhone } from "react-icons/fa";
-import { IoMdMail, IoMdMenu } from "react-icons/io";
-import constants from "@/utils/constants/constans.json";
-import NavMobileMenu from "./Mobile/NavMobileMenu";
+'use client';
+import { useState } from 'react';
+import { FaPhone } from 'react-icons/fa';
+import { IoMdMail, IoMdMenu } from 'react-icons/io';
+import constants from '@/utils/constants/constans.json';
+import NavMobileMenu from './Mobile/NavMobileMenu';
+import { sendGAEvent } from '@next/third-parties/google';
 
 const NavbarFixedBar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -15,13 +16,13 @@ const NavbarFixedBar = () => {
       className={`NAVBAR_FIXED_TOP_BAR fixed w-full h-[var(--emerg-bar-h-mobile)] md:h-[var(--emerg-bar-h-desktop)] bg-black flex flex-col md:flex-row justify-center items-center p-2 gap-2 md:gap-4 lg:gap-8 text-white z-[30]`}
     >
       <div
-        className="absolute top-4 right-4 bg-gray-400 text-black rounded-md p-1 shadow-lg shadow-gray-400 md:hidden cursor-pointer"
+        className='absolute top-4 right-4 bg-gray-400 text-black rounded-md p-1 shadow-lg shadow-gray-400 md:hidden cursor-pointer'
         onClick={handleMobileMenu}
       >
         <IoMdMenu size={25} />
       </div>
       <a
-        className="flex justify-center items-center gap-2 hover:bg-gray-400 px-2 py-1 rounded-lg hover:text-black duration-500"
+        className='flex justify-center items-center gap-2 hover:bg-gray-400 px-2 py-1 rounded-lg hover:text-black duration-500'
         href={`tel:${constants.phone}`}
         title={`Phone Contact | ${constants.phone} | ${constants.aria_label}`}
         aria-label={`Phone Contact | ${constants.phone} | ${constants.aria_label}`}
@@ -30,7 +31,7 @@ const NavbarFixedBar = () => {
         <span> {constants.phone}</span>
       </a>
       <a
-        className="flex justify-center items-center gap-2 hover:bg-gray-400 px-2 py-1 rounded-lg hover:text-black duration-500"
+        className='flex justify-center items-center gap-2 hover:bg-gray-400 px-2 py-1 rounded-lg hover:text-black duration-500'
         href={`mailto:${constants.email}`}
         title={`Email Contact | ${constants.email} | ${constants.aria_label}`}
         aria-label={`Email Contact | ${constants.email} | ${constants.aria_label}`}
@@ -39,10 +40,22 @@ const NavbarFixedBar = () => {
         <span> {constants.email}</span>
       </a>
       <a
-        className="NAVBAR_EMERGENCY_CONTACT_CONTAINER flex justify-center items-center gap-2 bg-red-600 font-bold uppercase px-2 py-1 rounded-md text-center hover:text-black duration-500 mt-2 md:mt-0"
+        className='NAVBAR_EMERGENCY_CONTACT_CONTAINER flex justify-center items-center gap-2 bg-red-600 font-bold uppercase px-2 py-1 rounded-md text-center hover:text-black duration-500 mt-2 md:mt-0'
         href={`tel:${constants.emergency_phone}`}
         title={`Emergency | 24/7 | Phone Contact | ${constants.emergency_phone} | ${constants.aria_label}`}
         aria-label={`Emergency | 24/7 | Phone Contact | ${constants.emergency_phone} | ${constants.aria_label}`}
+        onClick={() =>
+          sendGAEvent(
+            'emergency_call_button',
+            'click',
+            'main_navbar_emergency_call_button'
+          )
+        }
+
+        //  Parameters:
+        // "emergency": This might represent the event category or type.
+        // "click": This is likely the event action, indicating what type of interaction occurred.
+        // "emergency-contact": This could be a label or additional descriptor for the event.
       >
         <p>
           emergency 24/7 <span> {constants.emergency_phone}</span>
